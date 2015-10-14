d177897bd116723b0a5f0887a4d180c3b98a7b47
Henning Perl <perl@cs.uni-bonn.de>
2015-10-14 08:56:33 +0200

    add tests for locale

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe PagesController, type: :controller do
+describe PagesController do
+  describe "localization" do
+    context "no locale set in params" do
+      before { get :show, id: 'index' }
+      it "defaults to german locale" do
+        expect(I18n.locale).to eq(:de)
+      end
+    end
 
+    context "locale set to english in params" do
+      before { get :show, id: 'index', locale: 'en' }
+      it "sets english locale" do
+        expect(I18n.locale).to eq(:en)
+      end
+    end
+  end
 end
